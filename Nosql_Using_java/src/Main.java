1fd9860cb0e295d83cd6ac71f7f5433eee8749ac
h-sumeet <124994281+h-sumeet@users.noreply.github.com>
2023-03-06 12:06:56 +0530

    Update Main.java

--- Nosql_Using_java/src/Main.java
+++ Nosql_Using_java/src/Main.java
@@ -18,28 +18,28 @@ public class Main {
 
         // Select some data from the database
         JSONObject result1 = db.select("key1");
-        System.out.println(result1); // prints {"name":"Alice","age":30}
+        System.out.println(result1); // prints {"name":"Sumeet","age":21}
 
         JSONObject result2 = db.select("key2");
-        System.out.println(result2); // prints {"name":"Bob","age":40}
+        System.out.println(result2); // prints {"name":"Rohit","age":40}
 
         // Update some data in the database
         JSONObject newData1 = new JSONObject();
-        newData1.put("name", "Alice Smith");
-        newData1.put("age", 31);
+        newData1.put("name", "Sumeet Haldar");
+        newData1.put("age", 21);
         db.update("key1", newData1);
 
         JSONObject newData2 = new JSONObject();
-        newData2.put("name", "Bob Johnson");
+        newData2.put("name", "Rohit Haldar");
         newData2.put("age", 41);
         db.update("key2", newData2);
 
         // Select the updated data from the database
         JSONObject updatedResult1 = db.select("key1");
-        System.out.println(updatedResult1); // prints {"name":"Alice Smith","age":31}
+        System.out.println(updatedResult1); // prints {"name":"Sumeet Haldar","age":21}
 
         JSONObject updatedResult2 = db.select("key2");
-        System.out.println(updatedResult2); // prints {"name":"Bob Johnson","age":41}
+        System.out.println(updatedResult2); // prints {"name":"Rohit Haldar","age":41}
 
         // Delete some data from the database
         db.delete("key1");
